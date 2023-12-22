f556a4fcb9b950b6eb58d47704645aa87ad81bf1
ChrisReid93 <101946270+ChrisReid93@users.noreply.github.com>
2023-12-21 22:13:25 -0500

    Test world and background music

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,20 +3,23 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+const CHAR_WIDTH = 16
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 # Use "@onready" for AnimationPlayer to enable get_node
 @onready var anim = $AnimationPlayer
-var can_crouch
 var can_jump = false
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		can_crouch = false
+		
+	# Prevent player from moving left off screen
+	if position.x < CHAR_WIDTH:
+		position.x = CHAR_WIDTH
 		
 	# Coyote Time setup. Check if player is on floor, and let them jump. If they fall (i.e. don't jump) then start CoyoteTime.
 	if is_on_floor():
@@ -24,10 +27,6 @@ func _physics_process(delta):
 	# Keep restarting CoyoteTimer when player is on ground. Once player leaves ground and can_jump then start timer.
 	elif can_jump == true and $CoyoteTimer.is_stopped():
 		$CoyoteTimer.start()
-
-#	# Handle crouch.
-#	if Input.is_action_pressed("crouch") and can_crouch == true:
-#		crouch()
 
 	# Handle falling
 	if not is_on_floor():
@@ -54,12 +53,8 @@ func _physics_process(delta):
 	# Flip animation depending on direction
 	if direction == -1:
 		$AnimatedSprite2D.flip_h = true
-		can_crouch = false
 	elif direction == 1:
 		$AnimatedSprite2D.flip_h = false
-		can_crouch = false
-	elif direction == 0:
-		can_crouch = true
 	
 	# If player is moving in X direction, move in direction at constant SPEED and play "Run" animation
 	if direction:
@@ -71,7 +66,7 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		# Check if player is on ground. If so, play "Idle" animation.
-		if velocity.y == 0 and not Input.is_action_pressed("crouch"):
+		if velocity.y == 0:
 			anim.play("Idle")
 
 	move_and_slide()
@@ -82,15 +77,6 @@ func jump():
 	anim.play("Jump")
 	can_jump = false
 
-# Create crouch function
-func crouch():
-	anim.play("Crouch")
-
 
 func _on_coyote_timer_timeout():
 	can_jump = false
-	print("CANNOT JUMP")
-
-
-func _on_jump_timer_timeout():
-	print("JUMP TIMER TIMED OUT")
